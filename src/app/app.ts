33b9bd1d47236a64f04ff5c21bef9ba51ecbede7
Andres Santos <fsantos.development@gmail.com>
2026-08-04 10:49:42 -0500

    chore: Instalar PrimeNG y TailwindCSS, configuraciones de las librerias

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
+  template: `
+    <div class="flex justify-center items-center min-h-screen">
+      <p-button label="Hola PrimeNG!" (onClick)="click()"><i class="pi pi-home"></i></p-button>
+    </div>
+    <div class="bg-red-500 text-white p-4">Tailwind funcionando</div>
+    <div class="bg-primary text-primary-contrast p-4 rounded-lg">
+      PrimeUI + Tailwind funcionando
+    </div>
+  `,
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('FrontEndAngular');
+  click() {
+    alert('Funciona!');
+  }
 }
